aae56beec7dd1c6ae598bb4201e27a7b5ff26a83
Yuma Hamasaki <whiteuma.hq@gmail.com>
2022-06-19 17:45:29 +0900

    initialize commit

---
+++ app/src/main/java/com/meowneko/todoapp/src/activity/TodoApp.kt
@@ -0,0 +1,14 @@
+package com.meowneko.todoapp.src.activity
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class TodoApp : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+
+        
+    }
+}
